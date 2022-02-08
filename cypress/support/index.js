f63cd11ebe19ab5836ee3a32cf985e5e1d0acc91
Dilip solanki <dilipsolanki@cdnsol.com>
2022-02-08 16:51:29 +0530

    Cypress

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -33,3 +33,7 @@ import './assertions';
 Cypress.on('uncaught:exception', () => {
     return false;
 });
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from failing the test
+    return false
+})
