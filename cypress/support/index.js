dd9d1e89fa7e5f4a7daecd08a21fcc9109bc56ba
Dilip Solanki <dilipsolanki@cdnsol.com>
2022-04-01 10:26:45 +0530

    Update index.js
    #patch

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -29,6 +29,7 @@ after(() => {
 Cypress.on('uncaught:exception', () => {
     return false;
 });
+
 Cypress.on('uncaught:exception', (err, runnable) => {
     // returning false here prevents Cypress from failing the test
     return false
